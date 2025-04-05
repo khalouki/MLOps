b7e455e4b921ba54a5aeb539fd6b4f0b03dd1ee5
khalouki <abdelkhalkessaid1@gmail.com>
2025-04-05 02:24:19 +0000

    remove all file

--- src/generators.py
+++
@@ -1,7 +0,0 @@
-
-import pandas as pd
-
-def csv_line_generator(file_path):
-    """Générateur qui lit un CSV ligne par ligne"""
-    for chunk in pd.read_csv(file_path, chunksize=1):
-        yield chunk.iloc[0].to_dict()
